79b4339e648c91f0de066ede53aed6064c44c8c3
joalopez1206 <joalopez@ug.uchile.cl>
2023-07-07 18:45:45 -0400

    changed the testing method

--- testratio.py
+++ testratio.py
@@ -45,18 +45,50 @@ def generate_names_with_ratio(list_of_names, list_of_movies, n, r=0.7):
 #2. luego de generar los nombres, vamos a asumir lo siguiente
 # Notemos que con el test a realizar, podemos esperar que el valor 
 # de nombres que son positivos seran: (# valores que estan) + (# falsos positivos)
-# si nuestro filtro de bloom es bueno, la idea es que estos falsos tengan una probabilidad de 0.1
-# por lo tanto el numero esperado de falsos positivos es (# numero de peliculas) * 0.1
+# si nuestro filtro de bloom es bueno, la idea es que estos falsos tengan una probabilidad de fp
+# por lo tanto el numero esperado de falsos positivos es (# numero de peliculas) * fp
 # Entonces hay que alcanzar este ese valor aproximado! ie 
 # Para alcanzar el optimo solo basta con chequear que el numero de falsos positivos sea el dicho anteriromente
 # dicho esto podemos asumir para encontrar ese k y M optimos podemos asumir como hipotesis lo siguiente
 #
-#       H1: k es lineal con respecto a M ie: k = α M para algun α en un rango 
+#       H1: k es lineal con respecto a M ie: k = α M, es optimo para algun α en un rango 
+#           y esperamos que α = ln(2)/n donde n es el numero esperado de valores que tenemos para un filtro de bloom
 #
 # Ahora, por teoria esperamos que α = ln(2)/n; donde n = (# valores esperados en el filtro de bloom)
 # y como n = 93889 y fp = 0.1, esperamos que α ~ 7.382e-06 = 0.000007382
 # fijaremos un k para el test! y de ahí obtendremos m y luego mediremos la cantidad de falsos positivos!
 # la idea es ir mostrando varios k's hasta ver que converga hasta el numero esperado de 
+KOPT = 3
+MOPT = 449965
+
+def test_m_and_k_for_optimum(n, trials=3):
+    ks = list(range(1,6))
+    ms = list(range(440000, 460001, 500))
+    for k in ks:
+        false_pos_list = []
+        for m in ms:
+            bloom_filter, list_names, list_movies = load_test_case(k,m)
+            list_for_testing = generate_names_with_ratio(list_names, list_movies, n = n)
+            #Queremos encontrar el que da mas cercano al minimo, ie el que da 
+            # una cantidad de falsos positivos cercano a 
+            # fp*(# peliculas)
+            false_pos = 0
+            for _ in range(trials):
+                false_pos += test_and_msr_false_positives(bloom_filter, list_for_testing)
+            false_pos_mean = false_pos / trials
+            false_pos_list.append(false_pos_mean)
+        fig = plt.figure()
+        plt.plot(ms, false_pos_list, color='blue', marker='.')
+        ax = plt.gca()
+        ax.set_ylim([150,700])
+        plt.title(f"#falsos postivos con k = {k} vs M")
+        plt.ylabel(r"#falsos positivos")
+        plt.xlabel(r"Numero de bits (M)")
+        plt.savefig(f"figures/Figura_k-{k}.png")
+        print(f"Figura {k} listo!")
+
+
+
 
 
 def test_and_msr_false_positives(bloomFilter: BloomFilterWithoutKandM, list_of_movies):
@@ -66,33 +98,24 @@ def test_and_msr_false_positives(bloomFilter: BloomFilterWithoutKandM, list_of_m
             pos += 1
     return pos - len(list_of_movies) * 0.7 
     
-def test_with_alpha(alpha, n=10000, k = 10, trials=20):
-    m = int(k/alpha)
-    bloomFilter, list_of_names, list_of_movies = load_test_case(k ,m)
-    list_for_testing = generate_names_with_ratio(list_of_names=list_of_names, list_of_movies=list_of_movies, n=n)
-    false_pos = 0
-    for _ in range(trials):
-        false_pos += test_and_msr_false_positives(bloomFilter, list_for_testing)
-    false_pos_mean = false_pos / trials
+# def test_with_alpha(alpha, n=10000, k = 10, trials=20):
+#     m = int(k/alpha)
+#     bloomFilter, list_of_names, list_of_movies = load_test_case(k ,m)
+#     list_for_testing = generate_names_with_ratio(list_of_names=list_of_names, list_of_movies=list_of_movies, n=n)
+#     false_pos = 0
+#     for _ in range(trials):
+#         false_pos += test_and_msr_false_positives(bloomFilter, list_for_testing)
+#     false_pos_mean = false_pos / trials
     
-    return false_pos_mean
+#     return false_pos_mean
     
 
-def test_a_list_of_alphas(list_of_alphas, k=10):
-    res= []    
-    for alpha in list_of_alphas:
-        res += [test_with_alpha(alpha, k=k)]
-    return res
+# def test_a_list_of_alphas(list_of_alphas, k=10):
+#     res= []    
+#     for alpha in list_of_alphas:
+#         res += [test_with_alpha(alpha, k=k)]
+#     return res
 
 
 if __name__ == "__main__":
-    alphas = np.linspace(0.000001, 0.00001, num=20)
-    for k in range(2,23, 2):
-        y_vals = test_a_list_of_alphas(alphas, k = k)
-        fig = plt.figure()
-        plt.scatter(alphas, y_vals, color='blue')
-        plt.title(f"#falsos postivos con k = {k} vs α")
-        plt.ylabel(r"#falsos positivos")
-        plt.xlabel(r"$alpha$")
-        plt.savefig(f"figures/Figura_k-{k}.png")
-        print(f"Figura {k} listo!")
+    test_m_and_k_for_optimum(n=10000, trials=50)
